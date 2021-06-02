9124061d2033563cdcdc4c61b622586ceaaeaff2
Cristian Almiron <cristianalmiron1988@gmail.com>
2021-06-02 06:00:21 -0300

    Para arrancar el ejer 17 de matrices

---
+++ tudai2021/src/matriz/ejer17.java
@@ -0,0 +1,18 @@
+package matriz;
+
+/*
+ * Para el Ejemplo 10, hacer:
+	– cargar un arreglo ARR_SUMAS_COLUMNAS_PARES los índices de las columnas de MAT
+	cuya suma es par (-1 para completar si no hay).
+	– ordenar ARR_SUMAS_COLUMNAS_PARES de forma ascendente según las sumas de
+	columnas de MAT utilizando un método de ordenamiento.
+ * */
+
+public class ejer17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
